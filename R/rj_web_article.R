67d350860b5be01523b4d6faa2ba803dd878f493
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-10-14 12:33:34 +0200

    add lua filter

--- R/rj_web_article.R
+++ R/rj_web_article.R
@@ -3,7 +3,10 @@ rjournal_web_article <- function(...) {
 
   rmarkdown::output_format(
     knitr = NULL, # use base one
-    pandoc = list(args = args), # only modify args
+    pandoc = list(
+      args = args,
+      lua_filters = system.file("latex-pkg.lua", package = "rjdistill")
+    ),
     keep_md = NULL, # use base one
     clean_supporting = NULL, # use base one
     base_format = distill::distill_article(...)
