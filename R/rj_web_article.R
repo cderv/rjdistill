3087fd36b84c67a8cc33899eed419705d00dbf3e
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-10-14 12:54:14 +0200

    Add pdf rendering

--- R/rj_web_article.R
+++ R/rj_web_article.R
@@ -1,5 +1,27 @@
+#' @export
 rjournal_web_article <- function(...) {
   args <- c()
+
+  rmd_path <- NULL
+
+  post_knit <- function(metadata, input_file, runtime, ...) {
+    rmd_path <<- input_file
+    NULL
+  }
+
+  post_processor <- function(metadata, input_file, output_file, clean, verbose) {
+    if (!is.null(metadata$type)) {
+      callr::r(function(input){
+        rmarkdown::render(
+          input,
+          # output_format = "rticles::rjournal_article",
+          output_format = "rjdistill::rjournal_pdf_article"
+        )
+      }, args = list(input = rmd_path))
+    }
+    # return output file unchanged
+    output_file
+  }
 
   rmarkdown::output_format(
     knitr = NULL, # use base one
@@ -9,6 +31,8 @@ rjournal_web_article <- function(...) {
     ),
     keep_md = NULL, # use base one
     clean_supporting = NULL, # use base one
-    base_format = distill::distill_article(...)
+    post_knit = post_knit,
+    post_processor = post_processor,
+    base_format = distill::distill_article(theme = theme, ...)
   )
 }
