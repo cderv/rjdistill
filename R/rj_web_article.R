e396efdc054d5177cd27f5b7df54797d02e1cf38
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-10-15 13:17:06 +0200

    Add custom appendix

--- R/rj_web_article.R
+++ R/rj_web_article.R
@@ -7,12 +7,45 @@ rjournal_web_article <- function(...) {
   post_knit <- function(metadata, input_file, runtime, ...) {
     # save Rmd path for later use
     rmd_path <<- input_file
+
     NULL
   }
 
   pre_processor <- function(metadata, input_file, runtime, knit_meta, files_dir,
                             output_dir) {
-    rmarkdown::pandoc_include_args(in_header = system.file("rjdistill.html", package = "rjdistill"))
+
+    # Add custom appendix
+    data <- list()
+    if (!is.null(metadata$supplementary_materials)) {
+      data <- c(data, list(supp = metadata$supplementary_materials))
+    }
+    if (!is.null(metadata$CTV)) {
+      CTV <- sprintf("[%s](https://cran.r-project.org/view=%s)", metadata$CTV, metadata$CTV)
+      CTV <- paste(CTV, collapse = ", ")
+      data <- c(data, list(CTV = CTV))
+    }
+    if (!is.null(metadata$packages)) {
+      if (!is.null(metadata$packages$cran)) {
+        CRAN <- sprintf("[%s](https://cran.r-project.org/package=%s)", metadata$packages$cran, metadata$packages$cran)
+        CRAN <- paste(CRAN, collapse = ", ")
+        data <- c(data, list(CRAN = CRAN))
+      }
+      if (!is.null(metadata$packages$bioc)) {
+        BIOC <- sprintf("[%s](https://www.bioconductor.org/packages/%s)", metadata$packages$bioc, metadata$packages$bioc)
+        BIOC <- paste(BIOC, collapse = ", ")
+        data <- c(data, list(BIOC = BIOC))
+      }
+    }
+
+    template <- xfun::read_utf8(system.file("appendix.md", package = "rjdistill"))
+    appendix <- whisker::whisker.render(template, data)
+
+    input <- xfun::read_utf8(input_file)
+    xfun::write_utf8(c(input, "", appendix), input_file)
+    # Custom args
+    args <- rmarkdown::pandoc_include_args(in_header = system.file("rjdistill.html", package = "rjdistill"))
+
+    args
   }
 
   post_processor <- function(metadata, input_file, output_file, clean, verbose) {
